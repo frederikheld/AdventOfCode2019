e91902b0881d29fbec8c2e380751ebcf67b37428
Frederik Held <kontakt@frederikheld.de>
2019-12-09 02:42:25 +0100

    Completes day 04, challenge 02

--- challenges/day_04/tests/test_password_solver.py
+++ challenges/day_04/tests/test_password_solver.py
@@ -10,6 +10,18 @@ def test_password_solver():
         123444, 123445, 123446, 123447, 123448, 123449, 123455]
 
 
+def test_password_solver_no_large_groups():
+    assert password_solver(123444, 123460, False) == [
+        123445, 123446, 123447, 123448, 123449, 123455]
+
+    assert password_solver(123488, 123566, False) == [
+        123488, 123499, 123556, 123557, 123558, 123559, 123566]
+
+    # print(password_solver(122888, 123360))
+    # assert password_solver(122888, 123360, False) == [
+    #     122888, 122889, 122899, 122999, 123344, 123345, 123346, 123347, 123348, 123349, 123355, 123356, 123357, 123358, 123359]
+
+
 def test_check_six_digits():
     assert check_six_digits(123456) == True
     assert check_six_digits(12345) == False
@@ -34,10 +46,37 @@ def test_check_two_adjacent_are_same():
     assert check_two_adjacent_are_same(12344) == True
     assert check_two_adjacent_are_same(11111) == True
 
-    assert check_two_adjacent_are_same(11) == True
     assert check_two_adjacent_are_same(1) == False
+    assert check_two_adjacent_are_same(11) == True
 
     assert check_two_adjacent_are_same(12325) == False
+
+
+def test_check_two_adjacent_are_same_no_large_groups():
+    assert check_two_adjacent_are_same(12344, False) == True
+    assert check_two_adjacent_are_same(12234, False) == True
+    assert check_two_adjacent_are_same(11234, False) == True
+
+    assert check_two_adjacent_are_same(11222, False) == True
+    assert check_two_adjacent_are_same(11122, False) == True
+    assert check_two_adjacent_are_same(12233, False) == True
+
+    assert check_two_adjacent_are_same(11111, False) == False
+
+    assert check_two_adjacent_are_same(1, False) == False
+    assert check_two_adjacent_are_same(11, False) == True
+    assert check_two_adjacent_are_same(111, False) == False
+
+    assert check_two_adjacent_are_same(112, False) == True
+    assert check_two_adjacent_are_same(122, False) == True
+
+    assert check_two_adjacent_are_same(12325, False) == False
+
+    assert check_two_adjacent_are_same(123333, False) == False
+    assert check_two_adjacent_are_same(123334, False) == False
+
+    assert check_two_adjacent_are_same(1123456677, False) == True
+    assert check_two_adjacent_are_same(111123456677, False) == True
 
 
 def test_check_digits_never_decrease():
